f410c15813f1215e62951fd2895b82b17e6b3971
benbenxiong <1321335146@qq.com>
2021-09-27 11:43:39 +0800

    优化webpack打包

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-// import ElementPlus from "element-plus";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -10,7 +9,6 @@ import "@kangc/v-md-editor/lib/style/base-editor.css";
 import vuepressTheme from "@kangc/v-md-editor/lib/theme/vuepress.js";
 import "@kangc/v-md-editor/lib/theme/style/vuepress.css";
 
-import "element-plus/lib/theme-chalk/index.css";
 import "mavon-editor/dist/css/index.css";
 
 import Prism from "prismjs";
